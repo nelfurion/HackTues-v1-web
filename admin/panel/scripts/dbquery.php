aae81e91219f1baebef3fad3414bf21a047ca454
Radostin <angelovradostin@gmail.com>
2015-05-09 19:50:44 +0200

    Added basic admin panel + some functionalities
    Clicking on the menu items sends AJAX requests to the content handler,
    which uses the dbquery script to get the needed data(model), then
    handles it, and returns it to the view.

---
+++ admin/panel/scripts/dbquery.php
@@ -0,0 +1,67 @@
+<!--
+	Fetches the data, from the database,
+	 and sends it to the content handler.
+ -->
+
+<?php
+	function getData($type) {
+		/*this should be changed, to be compatible with the server db's
+		  shoud be able to create array with fields,
+		  to be selected in the query*/
+		  $dbname;
+		  $sql;
+		switch ($type) {
+			case 'news':
+				$dbname = 'news';
+				$sql = "SELECT id, name, content FROM articles";
+				break;
+			case 'competitors':
+				$dbname = 'competitors';
+				break;
+			case 'sponsors':
+				$dbname = 'sponsors';
+				break;
+			case 'user':
+				$dbname = 'admins';
+				break;
+			case 'teams':
+				$dbname = 'teams';
+				break;
+			case 'crew':
+				$dbname = 'crew';
+				break;	
+			default:
+				return "Bad data type!";
+				break;
+		}
+		$servername = 'localhost';
+		$username = 'root';
+		$password = '';
+		
+
+		//creates the connection
+		if (!$dbname) {
+			echo "Bad data type! Could not resolve db connection.";
+		}
+		$conn = new mysqli($servername, $username, $password, $dbname);
+
+		//check for errors
+		if ($conn->connect_error) {
+			die("Connection to database failed!" . $conn->connect_error);
+		}
+
+		//query the db
+		if (!$sql) {
+			//not sure if return or echo
+			return "Bad data type!";
+			echo "Bad data type! Could not resolve query.";
+		}
+
+		$result = $conn->query($sql);
+
+		return $result;
+
+		//close connection
+		mysqli_close($conn);
+	}
+ ?>
